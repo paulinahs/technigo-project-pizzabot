b256bce254b1415283c13310823a2140cf2bcb13
Matilda Brunemalm <matilda@technigo.io>
2024-06-13 13:21:00 +0200

    Initialize repo

---
+++ code/script.js
@@ -0,0 +1,19 @@
+// Start here
+
+// Step 1 - Welcome and introduction
+// Your code goes here
+alert(
+  `Welcome to our Javascript Pizzeria. Ready to Start? - Click 'OK' to begin.`
+)
+
+// Step 2 - Food choice
+// Your code goes here
+
+// Step 3 - Subtype choice
+// Your code goes here
+
+// Step 4 - Age
+// Your code goes here
+
+// Step 5 - Order confirmation
+// Your code goes here
